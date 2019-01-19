c380a96beabb3f933102d15df4cd8ae42ed74be7
lyubovrosato <lyubov.rosato@gmail.com>
2019-01-19 16:42:59 -0500

    Local change

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,5 +1,6 @@
 public class Calculator {
     public static double add(double num1, double num2){
+        System.out.println("Local change");
         return num1+num2;
 
     }
@@ -10,5 +11,6 @@ public class Calculator {
     public static double multiply(double num1, double num2){
         return num1*num2;
     }
+
 
 }
