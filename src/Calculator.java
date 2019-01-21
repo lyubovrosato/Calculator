732b47a48c5e20e84c8e74070e7d92c126cff328
lyubovrosato <46764680+lyubovrosato@users.noreply.github.com>
2019-01-21 14:23:53 -0500

    Update Calculator.java

--- src/Calculator.java
+++ src/Calculator.java
@@ -11,5 +11,5 @@ public class Calculator {
     public static double multiply(double num1, double num2){
         return num1*num2;     
     }
-//Testing Git without glasses
+//Testing Git without glasses successfull
 }
